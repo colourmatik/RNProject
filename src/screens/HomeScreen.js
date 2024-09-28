ea0dad627cae9ca083fd8a859c4ca1518a9aa406
Podolnyi Aleksandr <colourmatik811@gmail.com>
2024-09-28 14:40:53 +0300

    connection check

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,50 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { 
+  View, 
+  Text, 
+  Button, 
+  StyleSheet, 
+  Platform, 
+  ToastAndroid, 
+  Alert 
+} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 
 export default function HomeScreen({ navigation }) {
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+
+      if (state.isConnected) {
+        if (Platform.OS === 'android') {
+          ToastAndroid.show('Подключено к интернету', ToastAndroid.SHORT);
+        } else if (Platform.OS === 'ios') {
+          Alert.alert('Статус подключения', 'Подключено к интернету');
+        }
+      } else {
+        if (Platform.OS === 'android') {
+          ToastAndroid.show('Нет подключения к интернету', ToastAndroid.SHORT);
+        } else if (Platform.OS === 'ios') {
+          Alert.alert('Статус подключения', 'Нет подключения к интернету');
+        }
+      }
+    });
+
+    return () => {
+      unsubscribe(); 
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Добро пожаловать!</Text>
-      <Button title="Авторизация" onPress={() => navigation.navigate('Tabs')} color="tomato"/>
+      <Button 
+        title="Авторизация" 
+        onPress={() => navigation.navigate('Tabs')} 
+        color="tomato"
+      />
     </View>
   );
 }
